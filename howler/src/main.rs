64052faf9d65729eaabdf55a2117039b313d255f
charles jeremiah <charlesjeremiah89@yahoo.com>
2023-01-05 21:01:35 +0100

    .

---
+++ howler/src/main.rs
@@ -0,0 +1,25 @@
+use clap::{App, Arg, ArgMatches};
+use std::fs;
+use std::path::Path;
+
+fn get_args() -> ArgMatches<'static> {
+    App::new("Howler (upper-cases input)")
+        .version("0.1.0")
+        .author("Jeremiah Charles")
+        .about("Rust howler program")
+        .help("Input string or file")
+        .arg(Arg::with_name("text").required(true))
+        .arg(Arg::with_name("output").short("o").takes_value(true))
+        .get_matches()
+}
+fn main() {
+    let matches = get_args();
+    let text = matches.value_of("text").unwrap();
+    let content = if Path::new(text).exists() {
+        fs::read_to_string(text).expect("should exist")
+    } else {
+        text.to_string()
+    };
+
+    println!("{}", content)
+}

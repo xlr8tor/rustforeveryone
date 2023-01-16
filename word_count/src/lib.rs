6b2313b3f1592ffb9e1724a5222836939c29804b
charles jeremiah <charlesjeremiah89@yahoo.com>
2023-01-16 22:44:07 +0100

    headr

--- word_count/src/lib.rs
+++ word_count/src/lib.rs
@@ -1,5 +1,5 @@
 use clap::{App, Arg};
-use std::error::Error;
+use std::{error::Error, io::BufRead};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -7,7 +7,7 @@ pub struct Config {
     file: Vec<String>,
 }
 
-fn get_args(config: Config) -> MyResult<Config> {
+pub fn get_args() -> MyResult<Config> {
     let matches = App::new("Emulate wc (word count)")
         .version("0.1.0")
         .author("Jeremiah Charles")
@@ -18,9 +18,16 @@ fn get_args(config: Config) -> MyResult<Config> {
                 .value_name("FILE")
                 .takes_value(true)
                 .multiple(true),
+            
         )
         .get_matches();
     Ok(Config {
         file: matches.values_of_lossy("file").unwrap(),
     })
 }
+
+pub fn run(config: Config) -> MyResult<()> {
+    for 
+}
+
+fn open(filename: &str) -> MyResult<Box <dyn BufRead>> {}
--- word_count/src/main.rs
+++ word_count/src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = word_count::get_args().and_then(word_count::run) {
+        eprint!("{}", e);
+        std::process::exit(1);
+    }
 }
